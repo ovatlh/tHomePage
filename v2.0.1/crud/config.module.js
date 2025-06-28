2aab85ba8a24abd331e4fc47860db99bb3847e2f
Tavo López Hernández <61797363+ovatlh@users.noreply.github.com>
2025-06-28 16:12:54 -0600

    config.module.js update

--- v2.0.1/crud/config.module.js
+++ v2.0.1/crud/config.module.js
@@ -1,7 +1,6 @@
 import DB_SCHEMA from "../dbSchema.js";
 import indexedDBUtils from "../../libs/utils/indexedDBUtils.js";
 import tmodals from "../../libs/tmodals/v0.1.0/tmodals.module.js";
-import utils from "../../libs/utils/utils.module.js";
 
 import crudSITE from "./site.module.js";
 
